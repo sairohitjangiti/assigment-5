1e12859885f72fcad68bd3219bc05f8c783788b1
sairohitjangiti <100988691+sairohitjangiti@users.noreply.github.com>
2022-03-25 18:46:51 +0530

    Create 1.(a)0dd functi0ns iffe functi0n.js.js

---
+++ 1.(a)0dd functi0ns iffe functi0n.js.js
@@ -0,0 +1,8 @@
+//       IIFE functions//
+(function (){
+    let number=[1,2,3,4,5,6,7,8,9,10];
+    for(i=0;i<number.length;i++){
+        if(number[i]%2!==0){
+       console.log(number[i]);
+    } }
+}) ();
