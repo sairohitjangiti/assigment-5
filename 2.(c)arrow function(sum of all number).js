05d857f15d3e751511bc749d07e8d1e00e07d475
sairohitjangiti <100988691+sairohitjangiti@users.noreply.github.com>
2022-03-25 19:39:58 +0530

    Create 2.(c)arrow function(sum of all number).js

---
+++ 2.(c)arrow function(sum of all number).js
@@ -0,0 +1,11 @@
+  //arrow function
+    const sum=()=>{
+        let str=[50,45,78];
+           let sum=0;
+         for (let i = 0; i < str.length; i++) {
+           sum +=str[i];
+         }
+        console.log(sum);
+        };
+        sum();
+        
