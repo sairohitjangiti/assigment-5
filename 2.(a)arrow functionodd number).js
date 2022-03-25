a740ab5b3bd7d95e31eba77952bdd70917dc4e7e
sairohitjangiti <100988691+sairohitjangiti@users.noreply.github.com>
2022-03-25 19:37:26 +0530

    Create 2.(a)arrow functionodd number).js

---
+++ 2.(a)arrow functionodd number).js
@@ -0,0 +1,10 @@
+//arrow function
+    const odd=() =>{
+        let number=[1,2,3,4,5,6,7,8,9,10];
+            for(i=0;i<number.length;i++){
+                 if(number[i]%2!==0){
+               console.log(number[i]);
+            } 
+            }
+        };
+            odd();
