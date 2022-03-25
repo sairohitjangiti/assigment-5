4cfb4726c5058796c776fb9158589c5559655819
sairohitjangiti <100988691+sairohitjangiti@users.noreply.github.com>
2022-03-25 19:10:38 +0530

    Create 1.(e)palindrome IIFE function.js

---
+++ 1.(e)palindrome IIFE function.js
@@ -0,0 +1,11 @@
+//IIFE function
+(function(){
+    var myArray = ['dad', 'cecarar', 'honda', 'malayalam'];   
+   var b = myArray.filter(function(c,d,f){
+   var Cur = c.split('').reverse().join('');
+   if(c == Cur){
+   console.log( myArray[d] );
+   }
+   });
+   
+   })();
