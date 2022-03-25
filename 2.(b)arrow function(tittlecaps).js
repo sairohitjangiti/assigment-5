442fba5a399a56565c4afc7498a9d52bb62b4d09
sairohitjangiti <100988691+sairohitjangiti@users.noreply.github.com>
2022-03-25 19:38:38 +0530

    Create 2.(b)arrow function(tittlecaps).js

---
+++ 2.(b)arrow function(tittlecaps).js
@@ -0,0 +1,10 @@
+//arrow function
+  const tittlecaps=()=>{
+    let str=("hello world".toLowerCase().split(" "));
+ 
+  for (let i = 0; i < str.length; i++) {
+    str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
+  }
+  console.log(str.join(" "));
+ }; 
+ tittlecaps();
