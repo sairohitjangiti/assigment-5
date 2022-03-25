9abe13706c38434ac515e1fe02ceea6dc0172486
sairohitjangiti <100988691+sairohitjangiti@users.noreply.github.com>
2022-03-25 19:32:05 +0530

    Create 1.(g)remove duplicates anonymous function.js

---
+++ 1.(g)remove duplicates anonymous function.js
@@ -0,0 +1,10 @@
+//anonymous function
+ const remove=   function(){
+let chars = [1,2,3,1,5,5];
+let uniqueChars = [...new Set(chars)];
+
+console.log(uniqueChars);
+};
+remove();
+© 2022 GitHub, Inc.
+Terms
