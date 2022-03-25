772df862667f931a0e9a8c447f37547ee8f07f91
sairohitjangiti <100988691+sairohitjangiti@users.noreply.github.com>
2022-03-25 19:06:11 +0530

    Create 1.(c)sum of all number IIFE function.js

---
+++ 1.(c)sum of all number IIFE function.js
@@ -0,0 +1,10 @@
+//IIFE function
+   (function(){
+    let str=[50,45,78];
+   let sum=0;
+ for (let i = 0; i < str.length; i++) {
+   sum +=str[i];
+ }
+console.log(sum);
+})();
+© 2022 GitHub, Inc.
