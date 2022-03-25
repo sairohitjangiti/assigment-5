f7a1623278c29b5d6d33fdd137026915f875c85e
sairohitjangiti <100988691+sairohitjangiti@users.noreply.github.com>
2022-03-25 19:00:48 +0530

    Create title caps an0nym0us functi0n.js

---
+++ title caps an0nym0us functi0n.js
@@ -0,0 +1,10 @@
+//Anonymous function
+   function titleCase() {
+    let str=("hello world".toLowerCase().split(" "));
+ 
+  for (let i = 0; i < str.length; i++) {
+    str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
+  }
+  console.log(str.join(" "));
+}
+titleCase();
